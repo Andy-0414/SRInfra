48a198249adb813858a97ff17b5e2fc3ffdd21f3
Andy0414 <pjh8667@naver.com>
2018-06-09 18:48:03 +0900

    dijkstra

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-
+const Graph = require('node-dijkstra')
 const klunch = require('k-lunch')
+
 const options = {
     autoCode: true,
     autoDomain: true
@@ -15,15 +16,15 @@ app.get('/meal/:year/:m/:day/:lunch', (req, res) => { // 급식 가져오기 /me
         year: req.params.year,
         month: req.params.m,
         day: req.params.day,
-        time: req.params.lunch, // Breakfast = 1, Lunch = 2, Dinner = 3
+        time: req.params.lunch,
         name: '선린인터넷고등학교',
         phase: 4
     }
     klunch.getLunch(form, (err, output) => {
-        if (err) {
+        if (err) { // 없는 데이터일시
             res.send(null);
         }
-        else {
+        else { // 데이터가 있을시
             res.send(output)
         }
 
@@ -31,20 +32,26 @@ app.get('/meal/:year/:m/:day/:lunch', (req, res) => { // 급식 가져오기 /me
 
 });
 
-app.get('/time/:grade/:class', (req, res) => {
+app.get('/time/:grade/:class', (req, res) => { // 시간표 가져오기
     var g = req.params.grade;
     var c = req.params.class;
-    fs.readFile('data/timeTable/' + g + 'g.json', (err, data) => {
+    fs.readFile('data/timeTable/' + g + 'g.json', (err, data) => { // data/timeTable/ 에있는 json파일을 불러옴
         if (err) res.send(null);
         else {
             res.send({
-                result: JSON.parse(data)[c]
+                result: JSON.parse(data)[c] // json파일로 변환후 해당 반에 대한 시간표 전송
             });
         }
     })
 
 })
 
+app.get('/distance',(req,res)=>{
+    fs.readFile('data/mapData.json', (err, data) => { // data/ 에있는 mapData.json파일을 불러옴
+        const route = new Graph(JSON.parse(data));
+        // 최단거리 짜야함
+    })
+})
 
 app.listen(3030, () => {
     console.log('Server Open');

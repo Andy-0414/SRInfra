aac88957a086bb90a926248a667281ab44988417
Andy0414 <pjh8667@naver.com>
2018-06-12 22:13:04 +0900

    sql

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
-const app = express();
-const fs = require('fs');
-const Graph = require('node-dijkstra')
-const klunch = require('k-lunch')
+const app = express(); // 서버 모듈
+const fs = require('fs'); // 파일 입출력
+const Graph = require('node-dijkstra'); // 최단거리
+const klunch = require('k-lunch'); // 급식
+const bodyParser = require('body-parser'); // POST
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: 'http://58.145.101.15:3000:3306',
+    user: 'srinfra',
+    password: '1111',
+    database: 'SRinfra'
+});
 
+con.connect();
 const options = {
     autoCode: true,
     autoDomain: true
 }
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("data"));
 
 app.get('/meal/:year/:m/:day/:lunch', (req, res) => { // 급식 가져오기 /meal/년도/월/일/중식or석식
@@ -46,13 +57,21 @@ app.get('/time/:grade/:class', (req, res) => { // 시간표 가져오기
 
 })
 
-app.get('/distance',(req,res)=>{
+app.get('/distance', (req, res) => {
     fs.readFile('data/mapData.json', (err, data) => { // data/ 에있는 mapData.json파일을 불러옴
         const route = new Graph(JSON.parse(data));
         // 최단거리 짜야함
     })
 })
+app.get('/test', (req, res) => {
+    var sql = "INSERT INTO login (id, password) VALUES('testId', 'testPassword')";
+    con.query(sql, (err, result, fields)=>{
+        console.log('good');
+    })
+})
+app.post('/login', (req, res) => {
 
+})
 app.listen(3030, () => {
     console.log('Server Open');
 })
